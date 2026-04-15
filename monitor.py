2c4cf6e3af2756e477902d290521c98be3984da7
Nishit-Suresh <nishisureshis@gmail.com>
2026-04-15 14:31:12 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,6 @@
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.packet import ethernet, ipv4
+from pox.lib.recoco import Timer
 
 log = core.getLogger()
 
@@ -9,17 +9,38 @@ class Monitor(object):
         self.connection = connection
         connection.addListeners(self)
 
-    def _handle_PacketIn(self, event):
-        packet = event.parsed
+        self.last_bytes = 0
 
-        if not packet.parsed:
-            log.warning("Ignoring incomplete packet")
-            return
+        # Request stats every 5 seconds
+        Timer(5, self.request_stats, recurring=True)
 
-        # Check for IP packet
-        ip_packet = packet.find('ipv4')
-        if ip_packet:
-            log.info("IP Packet: %s -> %s", ip_packet.srcip, ip_packet.dstip)
+    def request_stats(self):
+        log.info("Requesting flow stats from switch...")
+        req = of.ofp_stats_request(body=of.ofp_flow_stats_request())
+        self.connection.send(req)
+
+    def _handle_FlowStatsReceived(self, event):
+        total_bytes = 0
+
+        # Sum only valid flows (avoid noise/spikes)
+        for stat in event.stats:
+            if stat.packet_count > 0:
+                total_bytes += stat.byte_count
+
+        log.info("Total Bytes from switch: %s", total_bytes)
+
+        # Calculate safe bandwidth
+        delta_bytes = total_bytes - self.last_bytes
+
+        if delta_bytes < 0:
+            delta_bytes = 0  # prevent negative spikes
+
+        bandwidth_kbps = (delta_bytes * 8) / (5 * 1000)  # 5 sec interval
+
+        log.info("Estimated Bandwidth: %.2f Kbps", bandwidth_kbps)
+
+        self.last_bytes = total_bytes
+
 
 def launch():
     def start_switch(event):
